1de7eff8e2beaba64a16ede1492558889c826be9
Mark Woo Yong Ming <wooym2@ta-15.cs.byu.edu>
2014-04-09 23:15:14 -0600

    no ebay

--- public/js/deal/unsub.js
+++ public/js/deal/unsub.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
 		type:"GET",
 		url:"http://quickpick.herokuapp.com/subscribers",
 		dataType: "jsonp",
+		cache: true,  // the API complains about the extra parameter
+	    data: {  // the parameters to add to the request
+	        format: 'json',
+	        action: 'query',
+	        titles: 'test'
+	    },
 		success: function (data) {
 	      console.log(data)
 	      alert(data);
